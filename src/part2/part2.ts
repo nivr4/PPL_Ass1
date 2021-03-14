8067e70279a91b158a3422142ce26e428ce890d3
benaomri <63296602+benaomri@users.noreply.github.com>
2021-03-14 20:26:19 +0200

    Up

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -3,10 +3,26 @@ import * as R from "ramda";
 const stringToArray = R.split("");
 
 /* Question 1 */
-export const countVowels = undefined;
+export const countVowels: (str:string) =>number = (str) => {
+
+    const len= (stringToArray(str).filter((t:string) =>
+    {if ('aeiou'.includes(t)) return true; })).length;
+    console.log("len",len)
+    if (len)  return len;
+    return 0;
+
+}
 
 /* Question 2 */
-export const runLengthEncoding = undefined;
+export const runLengthEncoding: (str:string) => string = (str) => {
+    const letters=stringToArray(str);
+    return R.toString((R.countBy(R.toLower)(letters)))
+}
 
 /* Question 3 */
-export const isPaired = undefined;
+export const isPaired:(str:string) =>boolean = (str)=>{
+    return true;
+
+};
+console.log(countVowels("hello"))
+console.log(runLengthEncoding("aaaabbbccd"))
