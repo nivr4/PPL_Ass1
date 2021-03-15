b4ac1a4fd79af04c105d296d1aa8f5aca0ff6a24
nivr4 <63297412+nivr4@users.noreply.github.com>
2021-03-15 17:03:21 +0200

    Update find.ts

--- src/part3/find.ts
+++ src/part3/find.ts
@@ -1,4 +1,7 @@
-import { Result, makeFailure, makeOk, bind, either } from "../lib/result";
+
+import R = require("ramda");
+
+import { Result, makeFailure, makeOk, bind, either,isOk, isFailure } from "../lib/result";
 
 /* Library code */
 const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
@@ -8,7 +11,16 @@ const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
     throw "No element found.";
 }
 
-export const findResult = undefined;
+export const findResult =<T>(pred: (x: T) => boolean, a: T[]):Result<T> =>{
+    try{
+        const ans =findOrThrow(pred,a);
+        return makeOk(ans);
+    }
+    catch (e){
+        return makeFailure(e)
+
+    } 
+}  
 
 /* Client code */
 const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
@@ -20,6 +32,20 @@ const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
     }
 }
 
-export const returnSquaredIfFoundEven_v2 = undefined;
+export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number>=> {
+  const res=(findResult(((x:number) => {return x%2==0}),a))
+  return bind(res,(x:number)=>{ return makeOk(x*x)})
 
-export const returnSquaredIfFoundEven_v3 = undefined;
+}
+
+export const returnSquaredIfFoundEven_v3 = (a: number[]): number|any => {
+    
+    const findResultNum = findResult((x:number) => {return x%2==0}, a)
+    return either(findResultNum,() => 
+    { if (isOk(findResultNum)) return findResultNum.value*findResultNum.value },()=>-1)
+  
+
+
+}
+console.log(findResult((x:number) => {return x%2==0}, [1,2,4]));
+console.log(returnSquaredIfFoundEven_v3([1,4,7]));
