db0d30e7376a7562ccb0f1dc7f1430d8b6169064
nivr4 <63297412+nivr4@users.noreply.github.com>
2021-03-15 12:27:02 +0200

    Update find.ts

--- src/part3/find.ts
+++ src/part3/find.ts
@@ -1,4 +1,6 @@
-import { Result, makeFailure, makeOk, bind, either } from "../lib/result";
+import { ok } from "node:assert";
+
+import { Result, makeFailure, makeOk, bind, either,isOk } from "../lib/result";
 
 /* Library code */
 const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
@@ -8,7 +10,14 @@ const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
     throw "No element found.";
 }
 
-export const findResult = undefined;
+export const findResult =<T>(pred: (x: T) => boolean, a: T[]):Result<T> =>{
+    for(let i=0; i< a.length; i++){
+        if(pred(a[i]))
+        return (makeOk(a[i]))
+    }
+    return makeFailure(" no such element exists")
+    
+}  
 
 /* Client code */
 const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
@@ -20,6 +29,24 @@ const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
     }
 }
 
-export const returnSquaredIfFoundEven_v2 = undefined;
+export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> => {
+//    const bindFun= R.bind(findResult(((x:number) => {return x%2==0}),a)
+//                     (res:Result<number>) => isOk(res)?makeOk(res.value*res.value):makeFailure("no such element") )
+  
+  const check=  ((res:Result<number>) => 
+        isOk(res)?makeOk(res.value*res.value):makeFailure("no such element")).bind(
+      (findResult(((x:number) => {return x%2==0}),a))
+   if(isOk(check))return check;
 
-export const returnSquaredIfFoundEven_v3 = undefined;
+
+
+}
+
+export const returnSquaredIfFoundEven_v3 = (a: number[]): number => {
+    const eitherNum= R.either((findResult(((x:number) => {return x%2==0}),a), );
+    
+
+
+}
+
+console.log(returnSquaredIfFoundEven_v2([1,3,7]));
